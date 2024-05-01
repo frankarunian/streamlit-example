9c49caa7bd77e0c1e31c52561f98160eaa85ed69
frankarunian <167736551+frankarunian@users.noreply.github.com>
2024-05-01 01:32:16 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,30 +1,14 @@
 import streamlit as st
 from transformers import pipeline
 
-# Title of the web application
-st.title('SumText: Simple Text Summarization App')
+st.title('SumText: Text Summarization App')
 
-# Subheader or instructions
-st.subheader('Enter the text you would like to summarize below:')
-
-# Text area for user input
-user_input = st.text_area("Text Input", "Type Here or Paste the text you want to summarize...", height=250)
-
-# Initialize the Hugging Face summarization pipeline
+user_input = st.text_area("Text Input", "Type here or paste the text you want to summarize...", height=300)
 summarizer = pipeline("summarization")
 
-# Button to trigger summarization
 if st.button('Summarize'):
     if user_input:
-        # Perform summarization
         summary = summarizer(user_input, max_length=150, min_length=30, do_sample=False)
-        # Display the results
-        st.subheader('Summary')
         st.write(summary[0]['summary_text'])
     else:
-        # Error message for no input
-        st.error("Please input some text to summarize.")
-
-# Optional: About section at the end
-st.sidebar.header('About SumText')
-st.sidebar.info('This application is powered by Streamlit and Hugging Face’s transformers library for easy and quick text summarization.')
+        st.error("Please enter some text to summarize.")
